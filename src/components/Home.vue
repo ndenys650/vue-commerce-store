c71b17229825f88388f9bfc2c8fabb02350a6b02
Nate Denys <nate_denys@yahoo.com>
2017-09-22 08:05:06 -0700

    Download vue cli, create vue webpack template folder structure, npm install vue-router

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>
+            <h1>{{msg}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+    
